0da467eb2caec8f70e7d751f842a2dd03c398511
AhmadJamal177 <agmadjamal123@gmail.com>
2026-06-02 01:36:02 +0530

    Add files via upload

---
+++ Week_1/Day_1/two_sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+
+            if (mp.containsKey(remaining)) {
+                return new int[]{mp.get(remaining), i};
+            }
+
+            mp.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
