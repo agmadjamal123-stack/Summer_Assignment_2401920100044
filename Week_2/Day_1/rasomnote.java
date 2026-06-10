0d4368edbcc7e942b875f776f663f899c05ab9bf
AhmadJamal177 <agmadjamal123@gmail.com>
2026-06-10 13:18:02 +0530

    Add files via upload

---
+++ Week_2/Day_1/rasomnote.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        for (char ch : magazine.toCharArray()) {
+            count.put(ch, count.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : ransomNote.toCharArray()) {
+            if (count.getOrDefault(ch, 0) == 0) {
+                return false;
+            }
+            count.put(ch, count.get(ch) - 1);
+        }
+
+        return true;
+    }
+}
